4f4b8c4e4877a4629321e6d7d12b92350571221d
Johnny-Morgan <jwjmorgan@gmail.com>
2021-03-01 09:33:40 +0000

    Add data to db

--- mongo.py
+++ mongo.py
@@ -22,6 +22,36 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
+""" Insert multipe documents """
+
+new_docs = [{
+    "first": "douglas",
+    "last": "adams",
+    "dob": "11/03/1952",
+    "gender": "m",
+    "hair_color": "grey",
+    "occupation": "writer",
+    "nationality": "british"
+}, {
+    "first": "terry",
+    "last": "pratchett",
+    "dob": "28/04/1948",
+    "gender": "m",
+    "hair_color": "not much",
+    "occupation": "writer",
+    "nationality": "british"
+}, {
+    "first": "george",
+    "last": "rr martin",
+    "dob": "20/09/1948",
+    "gender": "m",
+    "hair_color": "white",
+    "occupation": "writer",
+    "nationality": "american"
+}]
+
+coll.insert_many(new_docs)
+
 documents = coll.find()
 
 for doc in documents:
